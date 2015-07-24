8a74d075ca9f58ebd8ec72c0ef65864a252d039b
Santiago Oleas <soleas@gmail.com>
2015-07-24 12:29:36 -0400

    - Wrapped file download and unzip in Boolean test to see if zip file   exists to avoid unnecessary downloading - adjust features.txt descriptions to have a more meaningful   name to fulfill Tidy Data requirements - exclude all 'angle' readings from final output of measurements   because some of these have the term 'Mean' but not as a function   'mean()' as we are expected to keep

--- run_analysis.R
+++ run_analysis.R
@@ -2,6 +2,17 @@
 ## Course Project
 ## Santiago Oleas
 
+## Version summary while coding
+## v0 - Initial Version submitted to github
+## v1 - Added concluding statement to Requirement 5 in comments
+## v2 - Wrapped file download and unzip in Boolean test to see if zip file
+##      exists to avoid unnecessary downloading
+##    - adjust features.txt descriptions to have a more meaningful
+##      name to fulfill Tidy Data requirements
+##    - exclude all 'angle' readings from final output of measurements 
+##      because some of these have the term 'Mean' but not as a function
+##      'mean()' as we are expected to keep
+
 ## INSTRUCTIONS
 ## You will be required to submit: 
 ##     (1) a tidy data set as described below
@@ -18,7 +29,7 @@
 ## You should create one R script called run_analysis.R that does the 
 ## following. 
 ## Req 1 - Merges the training and the test sets to create one data set.
-## Req 2 - Extracts only the measurements on the mean and standard deviation 
+## Req 2 - Extracts only the measurements on the mean and standxard deviation 
 ##         for each measurement. 
 ## Req 3 - Uses descriptive activity names to name the activities in the data set
 ## Req 4 - Appropriately labels the data set with descriptive variable names. 
@@ -26,21 +37,26 @@
 ##         data set with the average of each variable for each activity and each
 ##         subject.
 
+## Set working directory
+        setwd('/Users/santiago/git/coursera/Getting and Cleaning Data/Assignment')
+
 ## Load important libraries
         library(dplyr)
 
 ## Get data from Internet
-        #Get file
-        fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" 
-        download.file(fileURL, destfile = "motionData.zip", method = "curl")
+        #Check if we already have file.  If we do not then
+        #proceed with downloading and unzipping
+        if(!file.exists("./motionData.zip")) {
+                #Get file
+                fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip" 
+                download.file(fileURL, destfile = "motionData.zip", method = "curl")
 
-        ##Unzip file
-        unzip("motionData.zip",overwrite=TRUE)
-
+                ##Unzip file
+                unzip("motionData.zip",overwrite=TRUE)
+        }
+        
 ## Req 1 - Merges the training and the test sets to create one data set.
-## Req 4 - Appropriately labels the data set with descriptive variable names. 
-##         I will do requirement 1 and 4 in this section of code by combining
-##         data sets with the appropriate variable names
+
         
         ##Observing the unzipped file I see that we now have a nest of folders that looks like:
         ## ./UCI HAR Dataset/
@@ -54,16 +70,16 @@
         
         ## ./UCI HAR Dataset/test/
         ##      contains files
-        ##      - subject_test.txt
-        ##      - X_test.txt
-        ##      - y_test
+        ##      - subject_test.txt    ***subject ID for each observation in X_test.txt
+        ##      - X_test.txt          ***detailed observations
+        ##      - y_test              ***activity ID for each observation in X_test.txt
         ## ./UCI HAR Dataset/test/Inertial Signals ***Can be ignored
         
         ## ./UCI HAR Dataset/train/
-        ##      contains files
-        ##      - subject_train.txt
-        ##      - X_train.txt
-        ##      - y_train
+        ##      contains files        
+        ##      - subject_train.txt   ***subject ID for each observation in X_train.txt
+        ##      - X_train.txt         ***detailed observations
+        ##      - y_train             ***activity ID for each observation in X_train.txt
         ## ./UCI HAR Dataset/train/Inertial Signals ***Can be ignored
 
         ## Each of train and test has 3 data files that must be combined.  They
@@ -79,7 +95,7 @@
         
         ## So this is how we have to put this together. Picture these like blocks 
         ## that we either stack on top of or put next to each other. In some cases
-        ## we force a label eg, subject_id or we force a column value ("train"
+        ## we force a label eg, "subject_id" or we force a column value ("train"
         ## and "test" to differentiate the data source)
         ## ["group"]["subject_id"     ]["activity_id"][features.txt$description]
         ## ["train"][subject_test.txt ][y_test.txt   ][X_test.txt  ]
@@ -100,7 +116,7 @@
                 ##features
                 ##Give column names of featureID and featureDesc
                 features <- read.table("./UCI HAR Dataset/features.txt",sep="",col.names=c("featureID","featureDesc"))
-        
+                        
                 ##activities
                 ##Give column names of activityID and activityDesc
                 activities <- read.table("./UCI HAR Dataset/activity_labels.txt",sep="",col.names=c("activityID","activityDesc"))
@@ -118,12 +134,6 @@
                 ## We have taken care of Requirement 4 by including the variable
                 ## names from the features$featuresDesc we previously loaded
                 testReadings <- read.table("./UCI HAR Dataset/test/X_test.txt", sep = "",col.names=features$featureDesc)
-        
-                        ## We have an issue, the column names were loaded and any
-                        ## special characters ended up with periods. Example:
-                        ## tBodyAcc.mean...X.  We will use gsub to replace the column
-                        ## names with equivalents excluding the perios
-                        names(testReadings) <- gsub("\\.","",names(testReadings))
         
                 ##Check that all three have the same row count.  IF they don't 
                 ##then something went wrong.
@@ -157,13 +167,8 @@
                 ## names from the features$featuresDesc we previously loaded
                 trainReadings <- read.table("./UCI HAR Dataset/train/X_train.txt", sep = "",col.names=features$featureDesc)
         
-                        ## We have an issue, the column names were loaded and any
-                        ## special characters ended up with periods. Example:
-                        ## tBodyAcc.mean...X.  We will use gsub to replace the column
-                        ## names with equivalents excluding the perios
-                        names(trainReadings) <- gsub("\\.","",names(trainReadings))
-        
-                ##Check that all three have the same row count.  IF they don't
+                        
+                ##Check that all three have the same row count.  If they don't
                 ##then something went wrong.
                 dim(trainSubject)
                 dim(trainActivity)
@@ -201,9 +206,14 @@
         ## - all mean measurements:  contains("mean")
         ## - all standard deviation measurements:  contains('std')
         ## - exclude 'meanFreq' since that is not the same as mean: -contains('meanFreq')
-        ## We will use dplyr's SELECT function usig the above list
-        motionDataMeanSTD <- select(motionData,group,subjectID,activityID,contains('mean'),contains('std'),-contains('meanFreq'))
-
+        ## - exclude all of the 'angle' measurements. These are measuring an
+        ##   angle between gravity and another measurement and sometimes the
+        ##   word 'Mean' is in these measurements but they are not an actual
+        ##   mean of an observation as requested.
+        
+        ## We will use dplyr's SELECT function using the above list
+        motionDataMeanSTD <- select(motionData,group,subjectID,activityID,contains('mean'),contains('std'),-contains('meanFreq'),-contains('angle'))
+        
         ###
         ### This concludes Requirement 2.
         ### motionDataMeanSTD successfully has the key variables and all 
@@ -243,13 +253,69 @@
         ###
         
 ## Req 4 - Appropriately labels the data set with descriptive variable names.
-        ## I actually took care of this during Requirement 1 when I had loaded
-        ## the data frame 'features' from the features.txt and used the 
-        ## description variable features$featureDesc as the col.names argument
-        ## when loading both X_train.txt and X_test.txt files
         
-        names(motionDataMeanSTDActivity)
+        ## We will now create feature descriptions that are meaningful
+        ##This is important to help us with obtaining a Tidy Data Set
+        ##The README.txt file helps us understand the variable names
+        ##here.
+        ## PREFIX
+        ## - if prefixed with 't' (time) we will rename as 'time'
+        ## - if prefixed with 'f' (frequency) we will rename 'freq'
+        ## MEASUREMENT
+        ## - the measurements do have some meaningful names
+        ##   and these do not need to be simplified. Examples 
+        ##   are BodyAcc, GravityAcc, BodyAccJerk, BodyGyro
+        ##   BodyGyroJerk, BodyAccMag, GravityAccMag, 
+        ##   BodyAccJerkMag, BodyGyroMag, BodyGyroJerkMag
+        ## FUNCTION ON MEASUREMENT
+        ## - the measurements have functions applied to them
+        ##   which we can identify with '-fn()', where 'fn' is 
+        ##   the function name that is prefixed with '-'.  These 
+        ##   mean(), std(), mad(), max(), min(), sma(), energy()
+        ##   igr(), entropy(), arCoeff(), correlation(), etc.
+        ##   However the only two we care about are the 
+        ##   mean stated as  '-mean()' and standard deviation 
+        ##   stated as '-std()' we will rename 'Mean' and 'Std' 
+        ##    respectively.
+        ## AXIS
+        ## - some measurements have an axis direction denoted with 
+        ##   '-X', '-Y', '-Z' and other variations.  However the
+        ##   mean() and std() measurements make use of the simple
+        ##   variation '-X', '-Y', '-Z' so it is only these three
+        ##   we will rename 
+        ## MISC
+        ## - there are other observation variations (see all 'angle'
+        ##   prefixed measurements) that are not covered by the above
+        ##   however since we will not need these in the final Tidy Data
+        ##   output we will not rename these and simply keep them as is.
+        ## EXAMPLES
+        ## - tBodyGyroJerk-mean()-X ==> timeBodyGyroJerkMeanX
+        ## - fBodyAccJerk-std()-Z   ==> freqBodyAccJerkStdZ
+        
+        ## taking the vector of names as columns made all special characters
+        ## disappear and get replaced with '.' but we don't need those so 
+        ## we will clear those.
+        names(motionDataMeanSTDActivity) <- gsub("\\.","",names(motionDataMeanSTDActivity))
+        
+        ## replace all leading 'f' with 'freq'
+        names(motionDataMeanSTDActivity) <- gsub("^f","freq",names(motionDataMeanSTDActivity))
+        
+        ## replace all leading 't' with 'time'
+        names(motionDataMeanSTDActivity)<- gsub("^t","time",names(motionDataMeanSTDActivity))
+        
+        ## replace '-mean()' with 'Mean'
+        names(motionDataMeanSTDActivity) <- gsub("mean","Mean",names(motionDataMeanSTDActivity))
+        
+        ## replace '-std()' with 'Std'
+        names(motionDataMeanSTDActivity) <- gsub("std","Std",names(motionDataMeanSTDActivity))
+        
         ## We clearly see variables/columns are all labeled
+        head(motionDataMeanSTDActivity,1)
+        
+        ###
+        ### This concludes Requirement 4.
+        ### tidy column names have been produced.
+        ###
         
 ## Req 5 - From the data set in Step 4, creates a second, independent tidy 
 ##         data set with the average of each variable for each activity and each
